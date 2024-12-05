1d6ded6b5dd91e0c0f7c5fb49ff09f62c7badf2b
Vicente Duarte <157262766+GManPT@users.noreply.github.com>
2024-12-05 04:56:25 +0000

    Versão final do codigo

--- proj1.cpp
+++ proj1.cpp
@@ -1,6 +1,3 @@
-// Created by: Leonardo Neves :)
-// Created on: 2024-12-04
-
 #include <cstdio>
 #include <iostream>
 #include <vector>
@@ -10,217 +7,178 @@
 
 using namespace std;
 
-int** TABLE_PTR;
+/// INPUT VARIABLES
+/// n: Size of given table
+/// m: Size of sequence
+/// result: Desired value
+/// table: Given table with n size
+/// sequence: Given sequence with m size
+int n, m, result;
+vector<vector<int>> table;
+vector<int> sequence;
 
-int N_MAX;
-int M_MAX;
-vector<int> MAIN_ARRAY;
-vector<vector<vector<int>>> DATABASE;
 
-void printDatabase () {
-    for (int a = 0; a < M_MAX; a++) {
-        for (int b = 0; b < M_MAX; b++) {
-            putchar('{');
-            for (int i : DATABASE[a][b]){
-                printf("%d,",i);
-            }
-            putchar('}');
-            putchar(' ');
-        }
-        putchar('\n');
-    }
-}
+/// DP VARIABLES
+vector<vector<vector<int>>> dp;
 
-// formato do inner vector apenas na 1a diagonal {0 (indicates we are in terminal case), value}
+
+/// Initialize dp table 1st diagonal.
+/// Note that {0, value} is the terminal case later used.
+/// @return none
 void initializeDataBase () {
-    for (int i = 0; i < M_MAX; i++)
-    {
-        DATABASE[i][i].push_back(3);
-        DATABASE[i][i].push_back(-1);
-        DATABASE[i][i].push_back(MAIN_ARRAY[i]);
+    for (int i = 0; i < m; i++) {
+        dp[i][i].push_back(3);
+        dp[i][i].push_back(-1);
+        dp[i][i].push_back(sequence[i]);
     }
 }
 
-// formato dos inner inner vectors {size,IsMax,[value,leftcol,leftposition,rightrow,rightposition],[...],[...]}
 
-// function to calculate the vector of a cell based on 2 other vectors and their positions on the table
-vector<int> merge(int row, int col) {
+/// Read input values from stdin
+/// @return none
+void readInput() {
+    // Fast I/O (Leaks memory)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    vector<int> newVec;
-    newVec.push_back(2); // current size
-    newVec.push_back(0); // isMax
-    
+    // Read m and n values
+    cin >> n >> m;
+
+    // Resize table, sequence and dp
+    table.resize(n, vector<int>(n));
+    sequence.resize(m);
+    dp.resize(m, vector<vector<int>>(m));
+
+
+    // Read table values
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> table[i][j];
+        }
+    }
+
+    // Read sequence values
+    for (int i = 0; i < m; i++) {
+        cin >> sequence[i];
+    }
+
+    // Read desired value
+    cin >> result;
+}
+
+
+/// Calculate the vector of a cell based on 2 other vectors
+/// and their positions on the table
+/// @param row: Row of the cell
+/// @param col: Column of the cell
+/// @return vector<int>: Calculated vector
+vector<int> merge (int row, int col) {
+    // 2 is current size and the 0 max value
+    vector<int> newVec = {2, 0};
+
     int leftSide = col - 1;
-    int rightSide = col; 
+    int rightSide = col;
+    int isIn;
 
-   
     while (rightSide > row) {
-        vector<int> right =  DATABASE[rightSide][col];
-        vector<int> left = DATABASE[row][leftSide];
-
+        vector<int> left = dp[row][leftSide];
+        vector<int> right = dp[rightSide][col];
 
         for (int countLeft = 2; countLeft < left[0]; countLeft += VEC_CONST) {
-
             for (int countRight = 2; countRight < right[0]; countRight += VEC_CONST) {
+                isIn = 0;
 
-                int isIn = 0;
+                // Check if the value is already in the vector
                 for (int countVec = 2; countVec < newVec[0]; countVec += VEC_CONST) {
-                    if (TABLE_PTR[left[countLeft] - 1][right[countRight] - 1] == newVec[countVec]) {
+                    if (table[left[countLeft] - 1][right[countRight] - 1] == newVec[countVec]) {
                         isIn = 1;
                         break;
                     }
                 }
 
-                if (isIn == 0)
-                {
+                // If it is not in the vector, add it
+                if (!isIn) {
                     newVec[0] += VEC_CONST;
-                    newVec.push_back(TABLE_PTR[left[countLeft] - 1][right[countRight] - 1]);
+                    newVec.push_back(table[left[countLeft] - 1][right[countRight] - 1]);
                     newVec.push_back(leftSide);
                     newVec.push_back(countLeft);
                     newVec.push_back(rightSide);
                     newVec.push_back(countRight);
                 }
 
-                if (((newVec[0] - 2) / VEC_CONST) > N_MAX)
-                {
-                    printf("OVERFLOW\n"); // if appears on the output, something went wrong
-                }
-
-                if (((newVec[0] - 2) / VEC_CONST) == N_MAX) // if the vector is complete
-                {
+                // If the vector is full, break
+                if (((newVec[0] - 2) / VEC_CONST) == n) {
                     newVec[1] = 1;
                     break;
                 }
+                
             }
 
-            if (newVec[1] == 1)
-            {
-                break;
-            }
+            if (newVec[1]) break;
         }
 
-        if (newVec[1] == 1) 
-        {
-            break;
-        }
-        
-
+        if (newVec[1]) break;
         rightSide--;
         leftSide--;
     }
-
-
     return newVec;
 }
 
 
+/// Solve the problem
+/// @return none
+void solve() {
+    int row;
+
+    for (int i = 1; i < m; i++) {
+        row = 0;
+
+        for (int col = i; col < m; col++) {
+            dp[row][col] = merge(row, col);
+            row++;
+        }
+    }
+}
+
+
+// Print the vector (final expression)
+/// @param row: Row of the cell
+/// @param col: Column of the cell
+/// @param pos: Position of the vector
+/// @return none
 void printVector(int row, int col, int pos) {
-
-   
-    if (DATABASE[row][col][1] == -1)
-    {
-        printf("%d", DATABASE[row][col][2]);
-    }
-    
+    // Base case: If the vector is terminal
+    if (dp[row][col][1] == -1) cout << dp[row][col][2];
     else {
-
-        putchar('(');
-        printVector(row, DATABASE[row][col][pos + 1], DATABASE[row][col][pos + 2]);
-        putchar(' ');
-        printVector(DATABASE[row][col][pos + 3], col, DATABASE[row][col][pos + 4]);
-        putchar(')');
+        cout << "(";
+        printVector(row, dp[row][col][pos + 1], dp[row][col][pos + 2]);
+        cout << " ";
+        printVector(dp[row][col][pos + 3], col, dp[row][col][pos + 4]);
+        cout << ")";
     }
+}
+
+
+/// Print the solution
+/// @return none
+void printSolution() {
+    for (int i = 2; i < dp[0][m - 1][0]; i += VEC_CONST) {
+        // If the result is found, print it
+        if (dp[0][m - 1][i] == result) {
+            cout << "1" << endl;
+            printVector(0, m - 1, i);
+            return;
+        }
+    }
+
+    // If the result is not found, print 0
+    cout << "0" << endl;
 }
 
 
 int main() {
-
-    // INPUT VARIABLES
-    int n, m, result;
-    if (scanf("%d%d", &n, &m) != 2) {
-        fprintf(stderr, "Error reading n and m\n");
-        return 1;
-    }
-    int** table = new int*[n]; 
-    for (int i = 0; i < n; i++) {
-        table[i] = new int[n];
-        for (int j = 0; j < n; j++) {
-            if (scanf("%d",&table[i][j]) != 1) {
-                fprintf(stderr, "Error reading n and m\n");
-                return 1;
-            }
-        }
-    }
-    vector<int> array(m);
-    for (int i = 0; i < m; i++) { // loop for input vector
-        if (scanf("%d",&array[i]) != 1) {
-            fprintf(stderr, "Error reading n and m\n");
-            return 1;
-        }
-    }
-    if (scanf("%d",&result) != 1) {
-        fprintf(stderr, "Error reading n and m\n");
-        return 1;
-    }
-    
-
-    // END OF INPUT VARIABLES
-
-    // formato dos inner vectors {size,(value,leftrow,leftcol,leftposition,rightrow,rightcol,rightposition),()}
-
-
-
-    TABLE_PTR = table;
-    vector<vector<vector<int>>> data(m, vector<vector<int>>(m));
-    DATABASE = data;
-    N_MAX = n;
-    M_MAX = m;
-    MAIN_ARRAY = array;
-
-    int row = 0;
+    readInput();
     initializeDataBase();
-
-    
-    for(int a = 1; a < m; a++){
-        row = 0;
-
-        for (int col = a; col < m; col++) {
-
-          
-            DATABASE[row][col] = merge(row,col);
-            row++;
-
-
-        }
-    }
-
-    
-    int valid = 0;
-    
-    for (int o = 2; o < DATABASE[0][M_MAX - 1][0]; o += VEC_CONST)
-    {
-        if (DATABASE[0][M_MAX - 1][o] == result)
-        {
-            valid = 1;
-            putchar('1');
-            putchar('\n');
-            printVector(0, M_MAX - 1, o);
-            putchar('\n');
-            break;
-        }
-    }
-
-    if (valid == 0) {
-        putchar('0');
-        putchar('\n');
-    }
-    
-
-    for (int i = 0; i < n; ++i) {
-        delete[] table[i];  // Free each row
-    }
-    delete[] table; 
-
-    return 0;
+    solve();
+    printSolution();
 }
-
